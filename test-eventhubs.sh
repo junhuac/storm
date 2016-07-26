a42d3aae8e3dbeed441e143ff7dc3d939fac8c17
Junhua Chang <junhuac@hotmail.com>
2016-07-26 19:32:54 +0000

    Add eventhubs test script.

---
+++ test-eventhubs.sh
@@ -0,0 +1,2 @@
+java -cp ./external/storm-eventhubs/target/storm-eventhubs-2.0.0-SNAPSHOT-jar-with-dependencies.jar com.microsoft.eventhubs.client.example.EventHubSendClient "zonar" "qb5otAE6g7vyyU60uBHbWS50B8+xBK7oeW+gUO9rixc=" "dtna-ns" "dtna" 0 100 10
+
